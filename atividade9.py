87b588e9b0c0f498550ab13be61880be22e194a5
ruanbrito3 <ruan.brito9@aluno.ce.gov.br>
2023-10-18 11:21:20 -0300

    first commit

---
+++ atividade9.py
@@ -0,0 +1,7 @@
+#Exercício Python 09: faça um software que vique entre erif2 numeros qual o maior
+n1 = int(input("digite seu número"))
+n2 = int(input("digite o outro número"))
+if (n1/n2) < 1 :
+ print ("menor")
+if (n1/n2) > 1 :
+    print ("maior")
